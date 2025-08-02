9594bee441439cd667c7ddf5e7fac7d3385a1234
Gustavo Almeida <Gustavo65_almeida@outlook>
2025-08-02 11:59:02 -0300

    feat: add dotenv

--- src/main/java/com/linkedrh/mvp/MvpApplication.java
+++ src/main/java/com/linkedrh/mvp/MvpApplication.java
@@ -1,5 +1,6 @@
 package com.linkedrh.mvp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MvpApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(MvpApplication.class, args);
 	}
 
